4fe2fab802e9dd5ab29841b49fe3a8399cfbba6b
Brandon Edens <brandonedens@gmail.com>
2020-07-08 23:46:16 -0700

    Add in a test of smol.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use futures::io;
+use futures::prelude::*;
+
+fn main() -> io::Result<()> {
+    smol::run(async {
+        let settings = mio_serial::SerialPortSettings::default();
+        let mut port = mio_serial::Serial::from_path("/dev/ttyACM0", &settings)?;
+        #[cfg(unix)]
+        port.set_exclusive(false)
+            .expect("Unable to set serial port exclusive to false");
+
+        let stdin = smol::reader(std::io::stdin());
+        let mut stdout = smol::writer(std::io::stdout());
+
+        let mut io = smol::Async::new(port)?;
+        let (port_r, mut port_w) = (&mut io).split();
+
+        future::try_join(io::copy(stdin, &mut port_w), io::copy(port_r, &mut stdout)).await?;
+
+        Ok(())
+    })
 }
